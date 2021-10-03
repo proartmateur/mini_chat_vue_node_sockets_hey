08744bca6ca7a61cf8182a5c24f7ca4e4de0d0b2
proartmateur <ennimandroid@gmail.com>
2021-10-03 14:42:00 -0500

    API Body Params validation

--- src/controllers/message.controller.ts
+++ src/controllers/message.controller.ts
@@ -5,20 +5,34 @@ import {
   Controller,
   Get,
   attachControllers,
-  Middleware, Post,
+  Middleware,
+  Post,
 } from '@decorators/express'
+import { body, validationResult, ValidationChain } from 'express-validator'
+
 import MessageFakeRepository from '../components/message/infrastructure/persistence/message_fake_repository'
+import MessageSchema from '../models/message_schema'
 
 
-const repo = new MessageFakeRepository();
+const repo = new MessageFakeRepository()
+
+const new_message_validations: any[] = [
+  body('id').isUUID(4),
+  body('user_name').isString(),
+  body('content').isString(),
+]
 
 @Controller('/')
 export class MessageController {
   private base_route: string
+
+
   constructor() {
     this.base_route = '/messages'
+
   }
 
+  // @ts-ignore
   @Get('/')
   async listAll(@Response() res) {
     const messages = await repo.list()
@@ -27,12 +41,35 @@ export class MessageController {
     })
   }
 
+
+  @Post('/', new_message_validations)
+  async newMessage(@Request() req, @Response() res) {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const rbody = req.body
+    try {
+      const message = new MessageSchema(rbody)
+      await message.save()
+      res.status(201).json({
+        message: 'message created!',
+      })
+    } catch (error) {
+      res.status(500).json({
+        message: 'Can not save the message',
+        error,
+      })
+    }
+  }
+
   @Post('/find')
-  async getData(@Request() req, @Response() res) {
+  async getMessage(@Request() req, @Response() res) {
     const content_query = req.body.content_query
     console.log(req)
     try {
-      const messages = await repo.find(content_query);
+      const messages = await repo.find(content_query)
       if (!messages) {
         res.status(404).json({
           error: 'No se encontraron mensajes: ' + content_query,
---
+++ src/models/message_schema.ts
@@ -0,0 +1,23 @@
+import { Schema, model } from 'mongoose'
+
+const MessageSchema = new Schema({
+  id: {
+    type: String,
+    required: [true, 'The id field is required'],
+    unique: true
+  },
+  user_name: {
+    type: String,
+    required: [true, 'The user_name field is required'],
+  },
+  content: {
+    type: String,
+    required: [true, 'The content field is required'],
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+export default model('Message', MessageSchema)
